50d0501faea4acfa20a771460068242dcb6f877c
baibaishuai <34979196+baibaishuai@users.noreply.github.com>
2018-04-19 11:09:25 +0800

    底部导航栏
    曹小静，快来看

---
+++ MyApplication3/app/src/main/java/com/example/myapplication/Fragment_fenlei.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by 伯明翰 on 2018/4/15.
+ */
+
+public class Fragment_fenlei extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.layout_fenlei,container,false);
+        return view;
+    }
+}
